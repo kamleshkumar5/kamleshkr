df7ee63b3780bd1e6960a125bc499ec555071ed0
kamleshkumar5 <95899945+kamleshkumar5@users.noreply.github.com>
2021-12-10 11:07:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["Kamlesh Kumar", "Web Developer", "Programmer Analyst Trainee in Cognizant"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+};
+
+var typed = new Typed(".title", options);
